1388474c447e11d6278cc71fe7352142391c5348
Michael Copley <mdcopley@Michaels-MacBook-Pro-3.local>
2016-09-12 17:06:07 -0400

    changing api route to heroku

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
 
-var url = 'http://howmanyrocks.ngrok.io/notrocks';
+var url = 'http://howmanyrocks.herokuapp.com/notrocks';
 
 
 
